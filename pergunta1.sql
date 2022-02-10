bf16ebeaf64243862dc06d5a1018a94d980e8851
SophiaLemosF <77020562+SophiaLemosF@users.noreply.github.com>
2022-02-10 13:01:18 -0300

    Update pergunta1.sql

--- pergunta1.sql
+++ pergunta1.sql
@@ -1,7 +1,7 @@
 -- Query que responde a pergunta 1 - Quantos contratos (tokens) estão sendo criados por bloco?
 SELECT  CONTRATO,
         BLOCO,
-        COUNT(*)  AS QUANTIDADE,
+        COUNT(CONTRATO)  AS QUANTIDADE,
         cast(FORMAT_DATE("%Y-%m-%d", CURRENT_DATE)as date)
 FROM `aerial-matrix-340319.smart_cto_eth.main`
 WHERE ANOMESIA = cast(FORMAT_DATE("%Y-%m-%d", CURRENT_DATE)as date)
