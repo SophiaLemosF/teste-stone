8738d9e0b065a7897640e0e0189e5316c5fe3a66
SophiaLemosF <77020562+SophiaLemosF@users.noreply.github.com>
2022-02-10 13:02:23 -0300

    Update pergunta2.sql

--- pergunta2.sql
+++ pergunta2.sql
@@ -1,7 +1,7 @@
 -- Query que responde a pergunta 2 - Quantos blocos estão sendo criados em um dia?
 SELECT  BLOCO,
         DATE(tmestamp) AS tmestamp,
-        COUNT(*)    AS QUANTIDADE,
+        COUNT(BLOCO)    AS QUANTIDADE,
         cast(FORMAT_DATE("%Y-%m-%d", CURRENT_DATE)as date)
 FROM `aerial-matrix-340319.smart_cto_eth.main`
 WHERE ANOMESIA = cast(FORMAT_DATE("%Y-%m-%d", CURRENT_DATE)as date)
